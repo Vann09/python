f5352ec295fa47963154d0068ba85e3aec5d04e0
Jose Ignacio Izquierdo <izquierdoesteban1@gmail.com>
2021-04-26 14:11:17 +0200

    ejercicio3

---
+++ 2_Sintaxis_y_Estructuras_de_Control/Ejercicio3.py
@@ -0,0 +1,6 @@
+num = int(input ("Dime un numero "))
+print (f"Tabla de multiplicar del {num}")
+
+for f in range (1,11):
+    multiplicacion = num * f
+    print (f"{num} x {f} = {multiplicacion}")
