2c40421f11de3f091c78166f95f59edbb1040e0d
Jose Ignacio Izquierdo <izquierdoesteban1@gmail.com>
2021-04-26 12:20:49 +0200

    actualización_v2

---
+++ 2_Sintaxis_y_Estructuras_de_Control/demo.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+print ("¿Cual es tu fecha de nacimiento?")
+dt = datetime.now().date()
+fecha = input ()
+dt2 = datetime.strptime(fecha, "%d-%m-%Y").date()
+años = dt.year - dt2.year
+if (años > 18):
+    print (f"Tienes {años} años, eres mayor de edad.")
+
+else:
+    edad = 18 - años
+    print (f"Te faltan {edad} años para ser mayor de edad.")
