805d63dd5bac3767c37fe4fb3cf126a9d9e7034b
Vann09 <izquierdoesteban1@gmail.com>
2021-05-06 18:05:27 +0200

    Updatetrasclase6may

--- 6._Acceso_Base_de_Datos/Ejercicios/11._Extraer_info.py
+++ 6._Acceso_Base_de_Datos/Ejercicios/11._Extraer_info.py
@@ -45,3 +45,26 @@ for n in listProductos:
     totalProd = int(n['UnitsInStock'])*float(n['UnitPrice'])
     totalStock += totalProd
 print (f"\nValor total de Stock:  {totalStock:1.2f}")
+
+# Coste o Valor de nuestro Stock utilizand map() y sum()
+TotalStock = sum(list(map(lambda x: float(x['UnitPrice']) * float(n['UnitsInStock']) ,productos.find())))
+print(f"\nValor del Stock: {TotalStock:1.2f}")
+
+# Coste o Valor de nuestro Stock utilizando la función aggregate()
+query = [
+    { '$match': { 'UnitsInStock' : { '$ne': '0' } } },
+    { '$addFields': {
+        'Price': { '$toDouble': '$UnitPrice' },
+        'Stock': { '$toInt': '$UnitsInStock' },
+        }
+    },
+    { '$group': {
+        '_id': 'Valor del Stock', 
+        'Total': { '$sum': { '$multiply': [ '$Price', '$Stock' ] } },
+        'Items': { '$sum' : 1 }
+        }
+    }      
+]
+
+listProductos = productos.aggregate(query)
+print(listProductos.next())
