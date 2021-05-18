a4075d4a3babfa2ce1eeba93cad35cbff83550ee
Vann09 <izquierdoesteban1@gmail.com>
2021-05-18 17:57:32 +0200

    Create tiposdatossimples.py

---
+++ ejercicios random/tiposdatossimples.py
@@ -0,0 +1,48 @@
+#Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
+#print ("¡Hola Mundo!")
+
+#Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable.
+#saludo = '¡Hola Mundo!'
+#print (saludo)
+
+#Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+#nombre = input ("¿Cómo te llamas? ")
+#print (f"¡Hola {nombre}!")
+#print ("¡Hola " + nombre + "!")
+
+#Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética .
+#maths = ((3+2)/(2*5))**2
+#print (maths)
+
+#Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.
+#hours = float(input ("¿Cuantas horas trabajas? "))
+#cost = float(input ("¿Cuanto cobras por hora? "))
+#print (f"{hours*cost} € cobras.")
+
+#Escribir un programa que lea un entero positivo,n, introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta n .
+#num = int(input("Dime un número: "))
+#print (f"La suma de los primeros numeros enteros desde 1 hasta {num} es {(num*(num+1))/2}.")
+
+#Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, y muestre por pantalla la frase Tu índice de masa corporal es <imc> donde <imc> es el índice de masa corporal calculado redondeado con dos decimales.
+#peso = float(input("Dime tu peso en kg: "))
+#altura = float (input("Dime tu altura en metros: "))
+#imc = peso/(altura)**2
+#print (f"Tu indice de masa corporal es {imc:2.2f}")
+
+#Escribir un programa que pida al usuario dos números enteros y muestre por pantalla la <n> entre <m> da un cociente <c> y un resto <r> donde <n> y <m> son los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la división entera respectivamente.
+#num1 = int (input("Dime un número entero: "))
+#num2 = int (input("Dime otro número: "))
+#print (f"{num1} entre {num2} da un cociente {num1//num2} y un resto {num1%num2}.")
+
+#Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el número de años, y muestre por pantalla el capital obtenido en la inversión.
+#dinero = float(input("Cuanto quieres invertir: "))
+#inter = float(input ("A que interés anual: "))
+#años = int (input("A cuantos años: "))
+#print (f"Obtendrás {dinero * (inter/100 + 1) ** años:1.2f} €.")
+
+#Una juguetería tiene mucho éxito en dos de sus productos: payasos y muñecas. Suele hacer venta por correo y la empresa de logística les cobra por peso de cada paquete así que deben calcular el peso de los payasos y muñecas que saldrán en cada paquete a demanda. Cada payaso pesa 112 g y cada muñeca 75 g. Escribir un programa que lea el número de payasos y muñecas vendidos en el último pedido y calcule el peso total del paquete que será enviado.
+
+
+
+
+
