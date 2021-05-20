0eeac7105f857eb3f8874da2b66d3a3e96145fdf
Vann09 <izquierdoesteban1@gmail.com>
2021-05-20 17:16:42 +0200

    exercises varios

---
+++ ejercicios random/cadenas.py
@@ -0,0 +1,21 @@
+#Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
+#name = input ("Dime tu nombre: ")
+#num = int(input("Dime un numero entero: "))
+#count = 0
+#while (count < num):
+    #count += 1
+    #print (name)
+
+#Escribir un programa que pregunte el nombre completo del usuario en la consola y después muestre por pantalla el nombre completo del usuario tres veces, una con todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo con la primera letra del nombre y de los apellidos en mayúscula. El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
+#name = input ("Dime tu nombre completo: ")
+#print (name.title())
+#print (name.lower())
+#print (name.upper())
+
+#Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla <NOMBRE> tiene <n> letras, donde <NOMBRE> es el nombre de usuario en mayúsculas y <n> es el número de letras que tienen el nombre.
+#name = input ("Dime tu user name: ")
+#print (f"{name.upper()} tiene {len(name)} letras.")
+
+#Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extension donde el prefijo es el código del país +34, y la extensión tiene dos dígitos (por ejemplo +34-913724710-56). Escribir un programa que pregunte por un número de teléfono con este formato y muestre por pantalla el número de teléfono sin el prefijo y la extensión.
+#tlfn = input ("Dime un teléfono (prefijo-número-extension): ")
+#print (f"El nº de teléfono es {tlfn[4:-3]}.")
--- ejercicios random/tiposdatossimples.py
+++ ejercicios random/tiposdatossimples.py
@@ -41,8 +41,26 @@
 #print (f"Obtendrás {dinero * (inter/100 + 1) ** años:1.2f} €.")
 
 #Una juguetería tiene mucho éxito en dos de sus productos: payasos y muñecas. Suele hacer venta por correo y la empresa de logística les cobra por peso de cada paquete así que deben calcular el peso de los payasos y muñecas que saldrán en cada paquete a demanda. Cada payaso pesa 112 g y cada muñeca 75 g. Escribir un programa que lea el número de payasos y muñecas vendidos en el último pedido y calcule el peso total del paquete que será enviado.
+#clown = 112
+#doll = 75
+#pclown = int(input ("Payasos a pedir: "))
+#pdoll = int(input ("Muñecas a pedir: "))
+#print (f"El peso total del pedido a enviar es de {(clown*pclown) + (doll*pdoll)} gramos")
 
+#Imagina que acabas de abrir una nueva cuenta de ahorros que te ofrece el 4% de interés al año. Estos ahorros debido a intereses, que no se cobran hasta finales de año, se te añaden al balance final de tu cuenta de ahorros. Escribir un programa que comience leyendo la cantidad de dinero depositada en la cuenta de ahorros, introducida por el usuario. Después el programa debe calcular y mostrar por pantalla la cantidad de ahorros tras el primer, segundo y tercer años. Redondear cada cantidad a dos decimales.
+#money = float(input("Dinero a depositar: "))
+#interes = 4
+#balance1 = money*(interes/100 + 1)
+#print(f"El balance del primer año es de {balance1:1.2f}")
+#balance2 = balance1*(interes/100 + 1)
+#print(f"El balance del primer año es de {balance2:1.2f}")
+#balance3 = balance2*(interes/100 + 1)
+#print(f"El balance del primer año es de {balance3:1.2f}")
 
-
+#Una panadería vende barras de pan a 3.49€ cada una. El pan que no es el día tiene un descuento del 60%. Escribir un programa que comience leyendo el número de barras vendidas que no son del día. Después el programa debe mostrar el precio habitual de una barra de pan, el descuento que se le hace por no ser fresca y el coste final total.
+#pan = 3.49
+#panp = 0.6
+#panpsell = int(input("Pan vendido del dia anterior: "))
+#print (f"El precio del pan es de {pan} €, si la barra no es del día se aplica un descuesto del {panp*100} %, por lo que su compra es de {panpsell*pan*(1-panp):1.2f} €.")
 
 
